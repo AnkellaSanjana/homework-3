c4e422534d7f41b861b72197182b9e8340f8a617
AnkellaSanjana <ankellasanjanareddyus@gmail.com>
2023-10-15 20:14:27 +0300

    added proper functionality

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/home.html');
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/bio', (req, res) => {
+    res.sendFile(__dirname + '/bio.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
